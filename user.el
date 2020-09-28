f0c8f95cbb8031ae32459120cc8ff615fe930acd
Alexey Khoroshev <aleksxor@gmail.com>
2020-09-28 22:17:39 +0300

    change osx control modifier

--- user.el
+++ user.el
@@ -16,7 +16,7 @@
 ;; mac os key bindings
 
 (when (eq system-type 'darwin) ;; mac specific settings
-  (setq mac-option-modifier 'alt)
+  (setq mac-control-modifier 'alt)
   (setq mac-command-modifier 'control)
   ;; (global-set-key [kp-delete] 'delete-char) ;; sets fn-delete to be right-delete
   )
